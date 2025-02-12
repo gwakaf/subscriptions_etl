6ce1e3b89a4c22bc4103aa3bac0e66dc45ad5611
gwakaf <yagovaya@gmail.com>
2025-02-12 10:40:44 -0800

    Updated subscription step transformations

--- spark_jobs/02_subscriptions_step.py
+++ spark_jobs/02_subscriptions_step.py
@@ -1,30 +1,29 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType, DecimalType, TimestampType
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType, TimestampType
 from pyspark.sql import functions as F
+from pyspark.sql.window import Window
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 spark = SparkSession.builder \
     .appName("media_app_subscriptions") \
     .enableHiveSupport() \
     .getOrCreate()
     
-# AWS Glue Database & Table Names
-# glue_database = "your_glue_database"
-# glue_table = "subscriptions"
 
 S3_BUCKET = "s3://subscriprions-emr-etl"
 S3_BUCKET_SSOT = f"{S3_BUCKET}/data/ssot"
-S3_BUCKET_OUTPUT = f"{S3_BUCKET}/output"
+S3_BUCKET_PROD = f"{S3_BUCKET}/prod"
 subscriptions_data = "subscriptions"
-# current_date = datetime.today()
-# current_year = current_date.year
-# current_month = current_date.month
-# current_day = current_date.day
-partition_date = sys.argv[1]  # {{ ds_nodash }}
 
-subscriptions_file_path_incoming =f"{S3_BUCKET_SSOT}/{subscriptions_data}_{current_year}{current_month}{current_day}.csv"
-subscriptions_file_path_existed = f"{S3_BUCKET_OUTPUT}/{subscriptions_data}"
+batch_date = sys.argv[1]  # {{ ds_nodash }}
+batch_date_dt = datetime.strptime(batch_date, "%Y%m%d")
+latest_date_dt = batch_date_dt - timedelta(days=1)
+latest_date = latest_date_dt.strftime("%Y%m%d")
+
+subscriptions_file_path_incoming =f"{S3_BUCKET_SSOT}/{subscriptions_data}_{batch_date}.csv"
+subscriptions_file_path_existed = f"{S3_BUCKET_PROD}/{subscriptions_data}/dt={latest_date}"
+subscriptions_file_path_latest = f"{S3_BUCKET_PROD}/{subscriptions_data}/dt={batch_date}"
 
 subscriptions_schema_incoming = StructType([
     StructField("user_id", IntegerType(), True),
@@ -46,10 +45,11 @@ subscriptions_schema_existed = StructType([
     StructField("create_timestamp", TimestampType(), True),
     StructField("update_timestamp", TimestampType(), True),
     StructField("subs_cancel_timestamp", TimestampType(), True),
-    StructField("eff_start_timestamp", TimestampType(), True),
-    StructField("eff_end_timestamp", TimestampType(), True),
+    StructField("eff_start_date", DateType(), True),
+    StructField("eff_end_date", DateType(), True)
 ])
 
+# Read incoming new data
 subscriptions_df_incoming = spark.read.csv(subscriptions_file_path_incoming,
                                 schema=subscriptions_schema_incoming,
                                 header=True)
@@ -57,74 +57,43 @@ subscriptions_df_incoming = spark.read.csv(subscriptions_file_path_incoming,
 # Load Existing Table from AWS Glue Catalog 
 subscriptions_df_existing = spark.read.format("parquet").load(subscriptions_file_path_existed)
 
+# Add Effective Start and End Dates to Incoming Data (For New Records)
+subscriptions_df_incoming = subscriptions_df_incoming.withColumn("eff_start_date", batch_date_dt) \
+    .withColumn("eff_end_date", F.lit(None).cast("date"))
+    
+# Union existing and incoming dataframes)
+df_union = subscriptions_df_existing.unionByName(subscriptions_df_incoming)
 
-# Step 1: Identify Records that Changed (Join on user_id & subs_id)
-df_joined = subscriptions_df_existing.alias("existing").join(
-    subscriptions_df_incoming.alias("incoming"),
-    ["user_id", "subs_id"],
-    "outer"
-)
+# Define Window filter changed records 
+window_row_num = Window.partitionBy("user_id", "subs_id").orderBy(F.col("subs_start_timestamp").desc())
 
-# Step 2: Identify changed records (price_plan_id or subs_end_timestamp changed)
-df_changed = df_joined.filter(
-    (F.col("existing.price_plan_id") != F.col("incoming.price_plan_id")) |
-    (F.col("existing.subs_end_timestamp") != F.col("incoming.subs_end_timestamp"))
-)
+# Apply Window Function to assign row numbers
+df_ranked = df_union.withColumn("row_number", F.row_number().over(window_row_num))
 
-# Step 3: Mark existing records as inactive (set eff_end_timestamp)
-df_existing_updates = df_changed.select(
-    F.col("existing.user_id"),
-    F.col("existing.price_plan_id"),
-    F.col("existing.subs_id"),
-    F.col("existing.subs_start_timestamp"),
-    F.col("existing.subs_end_timestamp"),
-    F.col("existing.create_timestamp"),
-    F.col("existing.update_timestamp"),
-    F.col("existing.subs_cancel_timestamp"),
-    F.col("existing.eff_start_timestamp"),
-    F.current_timestamp().alias("eff_end_timestamp")  # Mark record as inactive
-)
+# Apply filter to get the latest active records
+subscriptions_df_latest = df_ranked.filter((F.col("row_number") == 1) & F.col("subs_cancel_timestamp").isNull()) \
+    .drop("row_number")
 
-# Step 4: Insert a new version of the updated record
-df_new_records = df_changed.select(
-    F.col("incoming.user_id"),
-    F.col("incoming.price_plan_id"),
-    F.col("incoming.subs_id"),
-    F.col("incoming.subs_start_timestamp"),
-    F.col("incoming.subs_end_timestamp"),
-    F.col("incoming.create_timestamp"),
-    F.col("incoming.update_timestamp"),
-    F.col("incoming.subs_cancel_timestamp"),
-    F.current_timestamp().alias("eff_start_timestamp"),  # New start date
-    F.lit(None).cast("timestamp").alias("eff_end_timestamp")  # Active record
-)
-
-# Step 5: Get unchanged records (not in df_changed)
-df_unchanged = subscriptions_df_existing.join(
-    df_changed.select("user_id", "subs_id"), ["user_id", "subs_id"], "left_anti"
-)
-
-# Step 6: Merge all data (unchanged + updated old + new records)
-df_final = df_existing_updates \
-    .unionByName(df_new_records, allowMissingColumns=True) \
-    .unionByName(df_unchanged, allowMissingColumns=True)
+# Apply filter to get historical and inactive records    
+subscriptions_df_historic = df_ranked.filter((F.col("row_number") > 1) | F.col("subs_cancel_timestamp").isNotNull()) \
+    .drop("row_number")
+    
+# Update effective end date for historical records
+subscriptions_df_historic = subscriptions_df_historic.withColumn("eff_end_date", batch_date_dt)
 
 
-# Step 7: Write Updated Data Back to AWS S3 in Parquet (Partitioned by subs_start_timestamp)
-df_final.write \
+# Write the latest snapshot data to S3 storage
+subscriptions_df_latest.write \
     .format("parquet") \
     .mode("overwrite") \
-    .partitionBy("subs_start_timestamp") \
+    .partitionBy("price_plan_id") \
+    .save(subscriptions_file_path_latest)
+
+# Update historic snapshot data on S3 storage    
+subscriptions_df_historic.write \
+    .format("parquet") \
+    .mode("overwrite") \
+    .partitionBy("price_plan_id") \
     .save(subscriptions_file_path_existed)
 
-# subscriptions_df.write.partitionBy("create_date").mode("overwrite").parquet("s3://emr-dev-exp-079300184175/output/subscriptions/")
-# subscriptions_df.write.mode("overwrite").parquet("s3://emr-dev-exp-079300184175/output/subscriptions/create_date='2023-09-05'")
 
-
-
-
-# subscriptions_df.write.mode("overwrite").parquet("/Users/bilberry/DE/emr-test/output/subscriptions/create_date='2023-09-05'")
-# subscriptions_df.write.partitionBy("create_date").mode("overwrite").parquet("/Users/bilberry/DE/emr-test/output/subscriptions")
-
-# s3://emr-dev-exp-079300184175/output/transactions/
-# s3://emr-dev-exp-079300184175/output/users/
