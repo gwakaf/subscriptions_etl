4e090f1f23cdd7dbcc8ce91d33469f23026c7de5
gwakaf <yagovaya@gmail.com>
2025-02-13 13:08:54 -0800

    Updating transactions and users steps code

--- spark_jobs/03_transactions_step.py
+++ spark_jobs/03_transactions_step.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType, TimestampType
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType, TimestampType, DecimalType
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
 import sys
@@ -25,75 +25,28 @@ transactions_file_path_incoming =f"{S3_BUCKET_SSOT}/{transactions_data}_{batch_d
 transactions_file_path_existed = f"{S3_BUCKET_PROD}/{transactions_data}/dt={latest_date}"
 transactions_file_path_latest = f"{S3_BUCKET_PROD}/{transactions_data}/dt={batch_date}"
 
-transactions_schema_incoming = StructType([
+# Transactions are stored in fact tables and do not need SCD to be applied
+
+transactions_schema = StructType([
+    StructField("transaction_id", IntegerType(), True),
     StructField("user_id", IntegerType(), True),
-    StructField("price_plan_id", IntegerType(), True),
     StructField("subs_id", IntegerType(), True),
-    StructField("subs_start_timestamp", TimestampType(), True),
-    StructField("subs_end_timestamp", TimestampType(), True),
-    StructField("create_timestamp", TimestampType(), True),
-    StructField("update_timestamp", TimestampType(), True),
-    StructField("subs_cancel_timestamp", StringType(), True)
+    StructField("price_plan_id", IntegerType(), True),
+    StructField("transaction_amount", DecimalType(), True),
+    StructField("transaction_created_timestamp", TimestampType(), True),
+    StructField("transaction_updated_timestamp", TimestampType(), True),
+    StructField("transaction_status", StringType(), True)
 ])
 
-transactions_schema_existed = StructType([
-    StructField("user_id", IntegerType(), True),
-    StructField("price_plan_id", IntegerType(), True),
-    StructField("subs_id", IntegerType(), True),
-    StructField("subs_start_timestamp", TimestampType(), True),
-    StructField("subs_end_timestamp", TimestampType(), True),
-    StructField("create_timestamp", TimestampType(), True),
-    StructField("update_timestamp", TimestampType(), True),
-    StructField("subs_cancel_timestamp", TimestampType(), True),
-    StructField("eff_start_date", DateType(), True),
-    StructField("eff_end_date", DateType(), True)
-])
 
 # Read incoming new data
-transactions_df_incoming = spark.read.csv(transactions_file_path_incoming,
-                                schema=transactions_schema_incoming,
+transactions_df = spark.read.csv(transactions_file_path_incoming,
+                                schema=transactions_schema,
                                 header=True)
 
-# Load Existing Table from AWS Glue Catalog 
-transactions_df_existing = spark.read.format("parquet").load(transactions_file_path_existed)
-
-# Add Effective Start and End Dates to Incoming Data (For New Records)
-transactions_df_incoming = transactions_df_incoming.withColumn("eff_start_date", batch_date_dt) \
-    .withColumn("eff_end_date", F.lit(None).cast("date"))
-    
-# Union existing and incoming dataframes
-df_union = transactions_df_existing.unionByName(transactions_df_incoming)
-
-# Define Window filter changed records 
-window_row_num = Window.partitionBy("user_id", "subs_id").orderBy(F.col("subs_start_timestamp").desc())
-
-# Apply Window Function to assign row numbers
-df_ranked = df_union.withColumn("row_number", F.row_number().over(window_row_num))
-
-# Apply filter to get the latest active records
-transactions_df_latest = df_ranked.filter((F.col("row_number") == 1) & F.col("subs_cancel_timestamp").isNull()) \
-    .drop("row_number")
-
-# Apply filter to get historical and inactive records    
-transactions_df_historic = df_ranked.filter((F.col("row_number") > 1) | F.col("subs_cancel_timestamp").isNotNull()) \
-    .drop("row_number")
-    
-# Update effective end date for historical records
-transactions_df_historic = transactions_df_historic.withColumn("eff_end_date", batch_date_dt)
-
-
-# Write the latest snapshot data to S3 storage
-transactions_df_latest.write \
+# Write the data to S3 storage
+transactions_df.write \
     .format("parquet") \
     .mode("overwrite") \
     .partitionBy("price_plan_id") \
     .save(transactions_file_path_latest)
-
-# Update historic snapshot data on S3 storage    
-transactions_df_historic.write \
-    .format("parquet") \
-    .mode("overwrite") \
-    .partitionBy("price_plan_id") \
-    .save(transactions_file_path_existed)
-
-
--- spark_jobs/04_users_step.py
+++ spark_jobs/04_users_step.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType, TimestampType
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType, TimestampType, BooleanType
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
 import sys
@@ -25,26 +25,28 @@ users_file_path_incoming =f"{S3_BUCKET_SSOT}/{users_data}_{batch_date}.csv"
 users_file_path_existed = f"{S3_BUCKET_PROD}/{users_data}/dt={latest_date}"
 users_file_path_latest = f"{S3_BUCKET_PROD}/{users_data}/dt={batch_date}"
 
+
+#user_id,use_name,city,date_od_birth,email,mobile,created_timestamp,updated_timestamps
 users_schema_incoming = StructType([
     StructField("user_id", IntegerType(), True),
-    StructField("price_plan_id", IntegerType(), True),
-    StructField("subs_id", IntegerType(), True),
-    StructField("subs_start_timestamp", TimestampType(), True),
-    StructField("subs_end_timestamp", TimestampType(), True),
-    StructField("create_timestamp", TimestampType(), True),
-    StructField("update_timestamp", TimestampType(), True),
-    StructField("subs_cancel_timestamp", StringType(), True)
+    StructField("user_name", StringType(), True),
+    StructField("date_of_birth", DateType(), True),
+    StructField("email", StringType(), True),
+    StructField("mobile", StringType(), True),
+    StructField("user_created_timestamp", TimestampType(), True),
+    StructField("user_updated_timestamp", TimestampType(), True),
+    StructField("is_subs_active", BooleanType, False)
 ])
 
 users_schema_existed = StructType([
     StructField("user_id", IntegerType(), True),
-    StructField("price_plan_id", IntegerType(), True),
-    StructField("subs_id", IntegerType(), True),
-    StructField("subs_start_timestamp", TimestampType(), True),
-    StructField("subs_end_timestamp", TimestampType(), True),
-    StructField("create_timestamp", TimestampType(), True),
-    StructField("update_timestamp", TimestampType(), True),
-    StructField("subs_cancel_timestamp", TimestampType(), True),
+    StructField("user_name", StringType(), True),
+    StructField("date_of_birth", DateType(), True),
+    StructField("email", StringType(), True),
+    StructField("mobile", StringType(), True),
+    StructField("user_created_timestamp", TimestampType(), True),
+    StructField("user_updated_timestamp", TimestampType(), True),
+    StructField("is_subs_active", BooleanType, False),
     StructField("eff_start_date", DateType(), True),
     StructField("eff_end_date", DateType(), True)
 ])
@@ -65,17 +67,17 @@ users_df_incoming = users_df_incoming.withColumn("eff_start_date", batch_date_dt
 df_union = users_df_existing.unionByName(users_df_incoming)
 
 # Define Window filter changed records 
-window_row_num = Window.partitionBy("user_id", "subs_id").orderBy(F.col("subs_start_timestamp").desc())
+window_row_num = Window.partitionBy("user_id").orderBy(F.col("user_updated_timestamp").desc())
 
 # Apply Window Function to assign row numbers
 df_ranked = df_union.withColumn("row_number", F.row_number().over(window_row_num))
 
 # Apply filter to get the latest active records
-users_df_latest = df_ranked.filter((F.col("row_number") == 1) & F.col("subs_cancel_timestamp").isNull()) \
+users_df_latest = df_ranked.filter((F.col("row_number") == 1) & F.col("is_subs_active")==True) \
     .drop("row_number")
 
 # Apply filter to get historical and inactive records    
-users_df_historic = df_ranked.filter((F.col("row_number") > 1) | F.col("subs_cancel_timestamp").isNotNull()) \
+users_df_historic = df_ranked.filter((F.col("row_number") > 1) | F.col("is_subs_active")== False) \
     .drop("row_number")
     
 # Update effective end date for historical records
@@ -93,7 +95,7 @@ users_df_latest.write \
 users_df_historic.write \
     .format("parquet") \
     .mode("overwrite") \
-    .partitionBy("price_plan_id") \
+    .partitionBy("user_updated_timestamp") \
     .save(users_file_path_existed)
 
 
