9055170f70f9228511bee84ea7d24038170e2357
gwakaf <yagovaya@gmail.com>
2025-02-18 13:19:28 -0800

    Updating price plans step code applying SCD type 1

--- spark_jobs/01_price_plans_step.py
+++ spark_jobs/01_price_plans_step.py
@@ -21,10 +21,12 @@ latest_date_dt = batch_date_dt - timedelta(days=1)
 latest_date = latest_date_dt.strftime("%Y%m%d")
 
 price_plans_file_path_incoming =f"{S3_BUCKET_SSOT}/{price_plans_data}_{batch_date}.csv"
-price_plans_file_path_existed = f"{S3_BUCKET_PROD}/{price_plans_data}/dt={latest_date}"
 price_plans_file_path_latest = f"{S3_BUCKET_PROD}/{price_plans_data}/dt={batch_date}"
 
-price_plans_schema_incoming = StructType([
+# Because price  plans do not change often, and business always want the latest price plan data
+# SCD type 1 applied to this dataset
+
+price_plans_schema = StructType([
     StructField("price_plan_id", IntegerType(), True),
     StructField("price_plan_name", StringType(), True),
     StructField("price", DecimalType(), True),
@@ -32,59 +34,13 @@ price_plans_schema_incoming = StructType([
     StructField("price_plan_type", StringType(), True)
 ])
 
-price_plans_schema_existed = StructType([
-    StructField("price_plan_id", IntegerType(), True),
-    StructField("price_plan_name", StringType(), True),
-    StructField("price", DecimalType(), True),
-    StructField("payment_period", StringType(), True),
-    StructField("price_plan_type", StringType(), True),
-    StructField("eff_start_date", DateType(), True),
-    StructField("eff_end_date", DateType(), True)
-])
-
 # Read incoming new data
-price_plans_df_incoming = spark.read.csv(price_plans_file_path_incoming,
-                                schema=price_plans_schema_incoming,
+price_plans_df = spark.read.csv(price_plans_file_path_incoming,
+                                schema=price_plans_schema,
                                 header=True)
 
-# Load Existing Table from AWS Glue Catalog 
-price_plans_df_existing = spark.read.format("parquet").load(price_plans_file_path_existed)
-
-# Add Effective Start and End Dates to Incoming Data (For New Records)
-subscriptions_df_incoming = price_plans_df_incoming.withColumn("eff_start_date", batch_date_dt) \
-    .withColumn("eff_end_date", F.lit(None).cast("date"))
-
-# Union existing and incoming dataframes
-df_union = price_plans_df_existing.unionByName(price_plans_df_incoming)
-
-# Define Window filter changed records 
-window_row_num = Window.partitionBy("user_id", "subs_id").orderBy(F.col("subs_start_timestamp").desc())
-
-# Apply Window Function to assign row numbers
-df_ranked = df_union.withColumn("row_number", F.row_number().over(window_row_num))
-
-# Apply filter to get the latest active records
-price_plans_df_latest = df_ranked.filter((F.col("row_number") == 1) & F.col("subs_cancel_timestamp").isNull()) \
-    .drop("row_number")
-
-# Apply filter to get historical and inactive records    
-price_plans_df_historic = df_ranked.filter((F.col("row_number") > 1) | F.col("subs_cancel_timestamp").isNotNull()) \
-    .drop("row_number")
-    
-# Update effective end date for historical records
-price_plans_df_historic = price_plans_df_historic.withColumn("eff_end_date", batch_date_dt)
-
-
-# Write the latest snapshot data to S3 storage
-price_plans_df_latest.write \
+# Write the latest data to S3 storage
+price_plans_df.write \
     .format("parquet") \
     .mode("overwrite") \
-    .partitionBy("price_plan_id") \
     .save(price_plans_file_path_latest)
-
-# Update historic snapshot data on S3 storage    
-price_plans_df_historic.write \
-    .format("parquet") \
-    .mode("overwrite") \
-    .partitionBy("price_plan_id") \
-    .save(price_plans_file_path_existed)
